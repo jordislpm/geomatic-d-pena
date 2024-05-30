1ed4697e61e49547a7a1f0b732bcdfa7fc697641
jordislpm <jordislpm@gmail.com>
2024-05-30 18:39:07 -0400

    feat: HomeServices responsive

--- components/Home/HomeServices/HomeServices.tsx
+++ components/Home/HomeServices/HomeServices.tsx
@@ -8,31 +8,31 @@ import ButtonPrincipal from '@/components/share/ButtonPrincipal/ButtonPrincipal'
 
 
 function HomeServices() {
-  return (
-    <section className={styles.body}>
-        <div className={styles.body__title}>
-            <h2>NUESTROS SERVICIOS</h2>
-        </div>
-     {homeServicesData.map((service, index)=>(
-        <div key={service.title} 
-        className={`${index % 2 === 0 ? styles.service : styles.serviceReverse}`}>
-            <Image 
-            src={service.img}
-            alt={service.title}
-            className={styles.img}
-            width={500}
-            height={350}
-            />
-            <div className={styles.service__content}>
-                    <h4>{service.subtle}</h4>
-                    <h2>{service.title}</h2>
-                    <p>{service.description}</p>
-                    <ButtonPrincipal />
+    return (
+        <section className={styles.body}>
+            <div className={styles.body__title}>
+                <h2>NUESTROS SERVICIOS</h2>
             </div>
-        </div>
-       ))}
-    </section>
-  )
+            {homeServicesData.map((service, index) => (
+                <div key={service.title}
+                    className={`${index % 2 === 0 ? styles.service : styles.serviceReverse}`}>
+                    <Image
+                        src={service.img}
+                        alt={service.title}
+                        className={styles.img}
+                        width={500}
+                        height={350}
+                    />
+                    <div className={styles.service__content}>
+                        <h4>{service.subtle}</h4>
+                        <h2>{service.title}</h2>
+                        <p>{service.description}</p>
+                        <ButtonPrincipal />
+                    </div>
+                </div>
+            ))}
+        </section>
+    )
 }
 
 export default HomeServices
