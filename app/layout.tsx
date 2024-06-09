45035f7c9e12f0bbf72e1592f29d76a885a65488
jordislpm <jordislpm@gmail.com>
2024-06-09 11:18:02 -0400

    feat: add other google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       }}>
       </Script>
 
+      <Script 
+      id="gtm2" 
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JYV2HT1E1T')`
+      }}>
+      </Script>
+
       {/* <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
